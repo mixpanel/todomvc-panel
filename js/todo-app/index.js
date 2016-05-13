08157061eeccd104b160a1d12d1116bbefeaf974
teddddd <ted@mixpanel.com>
2016-05-13 16:23:57 -0700

    implement adding new todo

--- js/todo-app/index.js
+++ js/todo-app/index.js
@@ -2,6 +2,9 @@ import { Component } from 'panel';
 
 import template from './index.jade';
 
+const ENTER_KEY = 13;
+let todoId = 1;
+
 document.registerElement('todo-app', class extends Component {
   get $defaultState() {
     return {
@@ -45,6 +48,15 @@ document.registerElement('todo-app', class extends Component {
       editTodo: ev => {
         const id = Number(ev.target.parentElement.dataset.tid);
         this.update({editing: id});
+      },
+      newTodoKeypress: ev => {
+        if (ev.which === ENTER_KEY) {
+          const text = ev.target.value.trim();
+          if (text) {
+            ev.target.value = '';
+            this.update({todos: this.state.todos.concat({id: todoId++, text})});
+          }
+        }
       },
     });
   }
