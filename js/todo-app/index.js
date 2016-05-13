ca6e7d787f29f213ed3a39679d9276b804b184f8
t ddddddd <webdev@cmme.org>
2016-05-12 23:46:12 -0700

    move todo list to jade and put examples in defaultState

--- js/todo-app/index.js
+++ js/todo-app/index.js
@@ -3,6 +3,21 @@ import { Component } from 'panel';
 import template from './index.jade';
 
 document.registerElement('todo-app', class extends Component {
+  get $defaultState() {
+    return {
+      todos: [
+        {
+          text: 'Taste JavaScript',
+          completed: true,
+        },
+        {
+          text: 'Buy a unicorn',
+          completed: false,
+        },
+      ],
+    };
+  }
+
   get $template() {
     return template;
   }
